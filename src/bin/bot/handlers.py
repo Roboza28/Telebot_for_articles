d942945a2d162e1a9a6bf5f994b4c93fbc30718c
Roboza <alters_raindrop@mail.ru>
2024-03-13 00:06:34 +0300

    fix errors

--- src/bin/bot/handlers.py
+++ src/bin/bot/handlers.py
@@ -12,20 +12,25 @@ dict_with_users = {}
 
 @router.message(Command("start", 'help'))
 async def start_handler(message: Message):
+    if not (message.from_user.id in dict_with_users.keys()):
+        dict_with_users[message.from_user.id] = DataBase(message.from_user.id)
+
     if message.text == '/start':
         await message.answer(TEXT_WELCOME)
-        if not (message.from_user.id in dict_with_users.keys()):
-            dict_with_users[message.from_user.id] = DataBase(message.from_user.id)
+
     await message.answer(TEXT_HELP)
 
 
 @router.message(Command("sticker"))
-async def cmd_dice(message: Message):
+async def get_sticker(message: Message):
     await message.answer_sticker(choice(ID_STICKER_PACK))
 
 
 @router.message(Command('get_article'))
 async def get_article(message: types.Message):
+    if not (message.from_user.id in dict_with_users.keys()):
+        dict_with_users[message.from_user.id] = DataBase(message.from_user.id)
+
     database_for_user = dict_with_users.get(message.from_user.id)
     random_article = database_for_user.get_random_article_from_db()
     await message.answer(text=random_article)
@@ -33,6 +38,9 @@ async def get_article(message: types.Message):
 
 @router.message()
 async def message_handler(message: Message):
+    if not (message.from_user.id in dict_with_users.keys()):
+        dict_with_users[message.from_user.id] = DataBase(message.from_user.id)
+
     if message.text.find("http") != -1:
         database_for_user = dict_with_users.get(message.from_user.id)
         article_address = get_article_address(message.text)
