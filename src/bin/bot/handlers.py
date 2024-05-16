9a5fa813a82bb995f67b67dc317b7d778229c426
Roboza <alters_raindrop@mail.ru>
2024-05-16 15:32:34 +0300

    fixed a bug related to multiple links in a message

--- src/bin/bot/handlers.py
+++ src/bin/bot/handlers.py
@@ -1,10 +1,12 @@
-from src.bin.text import TEXT_HELP, TEXT_WELCOME, ID_STICKER_PACK
+from src.bin.text import TEXT_HELP, TEXT_WELCOME, ID_STICKER_PACK, TEXT_WARNING
 from src.bin.utils import get_article_address
 from src.bin.bot.class_DB import DataBase
 from aiogram.filters import Command
 from aiogram import types, Router
 from aiogram.types import Message
 from random import choice
+import re
+
 
 router = Router()
 dict_with_users = {}
@@ -41,8 +43,11 @@ async def message_handler(message: Message):
     if not (message.from_user.id in dict_with_users.keys()):
         dict_with_users[message.from_user.id] = DataBase(message.from_user.id)
 
-    if message.text.find("http") != -1:
+    if len(re.findall('http', message.text)) == 1:
         database_for_user = dict_with_users.get(message.from_user.id)
         article_address = get_article_address(message.text)
         info_about_writing = database_for_user.write_article_in_db(article_address)
         await message.answer(info_about_writing, disable_web_page_preview=True)
+
+    if len(re.findall('http', message.text)) > 1:
+        await message.answer(TEXT_WARNING)
